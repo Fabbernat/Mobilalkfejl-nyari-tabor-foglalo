83f271bb5dd0932fb62da64e93160d33895169b0
Fabian Bernat <fabbernat@gmail.com>
2025-03-20 19:38:15 +0100

    v0.2 english translation, renders well enough

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.nyaritaborfoglalo
+package com.example.myapplication
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.nyaritaborfoglalo.ui.theme.MyApplicationTheme
+import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Látogató")
+        Greeting("Android")
     }
 }

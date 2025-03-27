6cb4524da36dc1bad526dc7c0c6a2fe2fb647410
Fabian Bernat <fabbernat@gmail.com>
2025-03-27 19:48:33 +0100

    Add landscape and tablet qualifiers

--- app/src/main/java/com/example/mobilalkfejl_nyari_tabor_foglalo/MainActivity.java
+++ app/src/main/java/com/example/mobilalkfejl_nyari_tabor_foglalo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.mobilalkfejl_nyari_tabor_foglalo;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void login(View view) {
+        EditText userName = findViewById(R.id.editTextUserName);
+        EditText password = findViewById(R.id.editTextPassword);
+        if (userName.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+            userName.setError(null);
+            password.setError(null);
+        } else {
+            userName.setError("Invalid username or password");
+            password.setError("Invalid username or password");
+        }
+
+        String usernameStr = userName.getText().toString();
+        String passwordStr = password.getText().toString();
+
+        android.util.Log.i(LOG_TAG, "Bejelentkezett: " + usernameStr + ", jelszó: " + passwordStr);
     }
 }

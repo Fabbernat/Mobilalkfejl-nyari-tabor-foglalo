ac292566fa234aae9c64fca8f608c245c127e519
Fabian Bernat <fabbernat@gmail.com>
2025-04-14 22:41:37 +0200

    V8.2 !important update minSdkVersion

--- app/testclasses/build.gradle.kts
+++ app/testclasses/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.testclasses"
-        minSdk = 16
+        minSdk = 23
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"

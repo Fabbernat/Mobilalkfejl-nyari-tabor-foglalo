7d687bf7f01052f0ef59ed9a157fc40a03696b55
Fabian Bernat <fabbernat@gmail.com>
2025-04-24 19:51:36 +0200

    V8.add helper method classes

---
+++ app/src/main/java/com/example/mobilalkfejl_nyari_tabor_foglalo/adapters/NavigationListAdapter.java
@@ -0,0 +1,52 @@
+package com.example.mobilalkfejl_nyari_tabor_foglalo.adapters;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NavigationListAdapter extends RecyclerView.Adapter<NavigationListAdapter.ListViewHolder> {
+    private final List<NavigationListItem> items;
+    private final FragmentManager fragmentManager;
+
+    public NavigationListAdapter(List<NavigationListItem> items, FragmentManager fragmentManager) {
+        this.items = items;
+        this.fragmentManager = fragmentManager;
+    }
+
+    @NonNull
+    @Override
+    public ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        FrameLayout frameLayout = new FrameLayout(parent.getContext());
+        frameLayout.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        ));
+        frameLayout.setId(View.generateViewId());
+        return new ListViewHolder(frameLayout);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ListViewHolder holder, int position) {
+        NavigationListItemFragment fragment = new NavigationListItemFragment();
+
+        fragmentManager.beginTransaction()
+                .replace(holder.fragmentContainer.getId(), fragment)
+                .commit();
+
+        boolean hideBorder = position == items.size() - 1;
+
+        fragment.bindData(items.get(position), hideBorder);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    static class ListViewHolder extends RecyclerView.ViewHolder {
+        FrameLayout fragmentContainer;
+
+        public ListViewHolder(@NonNull FrameLayout itemView) {
+            super(itemView);
+            fragmentContainer = itemView;
+        }
+    }
+}

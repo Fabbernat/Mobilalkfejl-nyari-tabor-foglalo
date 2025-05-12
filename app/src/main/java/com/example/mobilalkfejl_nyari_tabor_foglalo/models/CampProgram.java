682c91025077d9f0db68fbc5d7b48d4a47c060b4
Fabian Bernat <fabbernat@gmail.com>
2025-05-12 23:14:01 +0200

    v2 add unversioned files to Am I done? Maybe?

---
+++ app/src/main/java/com/example/mobilalkfejl_nyari_tabor_foglalo/models/CampProgram.java
@@ -0,0 +1,16 @@
+package com.example.mobilalkfejl_nyari_tabor_foglalo.models;
+
+import java.util.Date;
+import java.util.List;
+
+public class CampProgram {
+    private String id;
+    private String title;
+    private String description;
+    private Date startTime;
+    private Date endTime;
+    private String location;
+    private List<String> equipmentNeeded;
+
+    // Constructor, getters, setters...
+}

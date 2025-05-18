4c6818cc4c4b70ca4317aa7a10c45cd270c92f08
Fabian Bernat <fabbernat@gmail.com>
2025-05-18 14:40:56 +0200

    v2.2.1

---
+++ app/src/main/java/com/fabianbernat/mobilalkfejl_nyari_tabor_foglalo/activities/UpcomingCampActivity.java
@@ -0,0 +1,4 @@
+package com.fabianbernat.mobilalkfejl_nyari_tabor_foglalo.activities;
+
+public class UpcomingCampActivity {
+}
---
+++ app/src/main/java/com/fabianbernat/mobilalkfejl_nyari_tabor_foglalo/utils/AlarmReceiver.java
@@ -0,0 +1,16 @@
+package com.fabianbernat.mobilalkfejl_nyari_tabor_foglalo.utils;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.fabianbernat.mobilalkfejl_nyari_tabor_foglalo.NotificationHandler;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        new NotificationHandler(context).send("Jelentkeztél már az idei nyári táborokra?");
+
+    }
+}
